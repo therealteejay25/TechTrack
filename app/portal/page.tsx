2a40020a37f8091cf60d8e3ef59957ee5e993929
therealteejay25 <therealteejay25@gmail.com>
2026-03-11 12:33:04 +0100

    hehe

--- app/portal/page.tsx
+++ app/portal/page.tsx
@@ -239,7 +239,7 @@ export default function PortalPage() {
               {otp.map((digit, index) => (
                 <input
                   key={index}
-                  ref={(el) => (inputRefs.current[index] = el)}
+                  ref={(el) => { inputRefs.current[index] = el; }}
                   type="text"
                   inputMode="numeric"
                   maxLength={1}
